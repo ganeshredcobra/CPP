5f23f1231f50f419acc493c2f28205e8a28af3cc
Ganesh <ganeshredcobra@gmail.com>
2015-09-24 13:04:02 +0530

    trying Cin and endl
    tr

--- Hello/ConsoleApplication1/ConsoleApplication1.cpp
+++ Hello/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,7 +3,12 @@
 
 int main()
 {
+	int x = 4;
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "First check" << std::endl;
+	std::cout << "The value of X is " << x << std::endl;
+	std::cout << "Enter a number : ";
+	std::cin >> x;
+	std::cout << "The number you entered is " << x << std::endl;
 	return 0;
 }
