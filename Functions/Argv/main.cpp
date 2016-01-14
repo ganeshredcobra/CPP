c0bcbdeef40c8464663fcd653f750bfbd3d20120
Ganesh <ganeshredcobra@gmail.com>
2016-01-14 08:52:13 +0530

    Fuctions with Arguments
    Fuctions with Arguments

---
+++ Functions/Argv/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintValues(int X, int Y)
+{
+    std::cout<<"The Value no One is: "<< X <<endl;
+    std::cout<<"The Value no Two is: "<< Y <<endl;
+}
+
+int main()
+{
+    PrintValues(6,7);
+    return 0;
+}
