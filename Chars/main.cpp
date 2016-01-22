b8f4a59859b82a4886391e5589740e8ef90b6086
Ganesh <ganeshredcobra@gmail.com>
2016-01-22 16:27:38 +0530

    Char type casting
    Char type casting

---
+++ Chars/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch(97);
+    cout << ch << endl;
+    cout << static_cast<int>(ch) << endl;
+    cout << ch << endl;
+    return 0;
+}
