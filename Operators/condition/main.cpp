6ece10eda478391fd34cd3b1377698066cd25720
ganeshredcobra <ganeshredcobra@gmail.com>
2016-02-08 23:41:19 +0530

    condition operator

---
+++ Operators/condition/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+   // Local variable declaration:
+   int x, y = 9;
+
+   x = (y < 10) ? 30 : 40;
+
+   cout << "value of x: " << x << endl;
+
+   return 0;
+}
