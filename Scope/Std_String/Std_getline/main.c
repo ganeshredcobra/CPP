951227c1ebb702577dffaf7247ab2c9b005aca1e
Ganesh <ganeshredcobra@gmail.com>
2016-02-26 10:11:36 +0530

    Example on usage of std::getline

---
+++ Scope/Std_String/Std_getline/main.c
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+/*  std::getline */
+ 
+int main()
+{
+    std::cout << "Enter your full name: ";
+    std::string name;
+    std::getline(std::cin, name); // read a full line of text into name
+ 
+    std::cout << "Enter your age: ";
+    std::string age;
+    std::getline(std::cin, age); // read a full line of text into age
+ 
+    std::cout << "Your name is " << name << " and your age is " << age;
+}
