e2d626b7dfd72d07e660472db8f516f171a54891
Ganesh <ganeshredcobra@gmail.com>
2016-02-26 10:08:58 +0530

    std::string example two

---
+++ Scope/Std_String/String_2/main.c
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+/* std::string */
+ 
+int main()
+{
+    std::cout << "Enter your full name: ";
+    std::string name;
+    std::cin >> name;
+ 
+    std::cout << "Enter your age: ";
+    std::string age;
+    std::cin >> age;
+ 
+    std::cout << "Your name is " << name << " and your age is " << age;
+}
