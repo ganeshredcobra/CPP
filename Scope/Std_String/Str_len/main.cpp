2d09adedbad0e79b60c383ee9c1d51fc02e54720
ganeshredcobra <ganeshredcobra@gmail.com>
2016-03-02 22:46:22 +0530

    String length

---
+++ Scope/Std_String/Str_len/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+/* String length */
+
+int main()
+
+{
+    std::string myName("Alex");
+    std::cout << myName << " has " << myName.length() << " characters\n";
+    return 0;
+}
