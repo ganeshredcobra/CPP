402bf395b7af6f2d7967e72f8140b213711b90fc
ganeshredcobra <ganeshredcobra@gmail.com>
2016-03-13 13:49:38 +0530

    If statements

---
+++ Control_Flow/IF/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/* If statements*/
+
+int main()
+{
+    std::cout << "Enter a number: ";
+    int x;
+    std::cin >> x;
+
+    if (x > 10)
+        std::cout << x << "is greater than 10\n";
+    else
+        std::cout << x << "is not greater than 10\n";
+
+    return 0;
+}
