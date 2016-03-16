03f5e3e80867a70b29c28abb597af8a19f16dd0d
ganeshredcobra <ganeshredcobra@gmail.com>
2016-03-16 23:35:05 +0530

    while loop

---
+++ Control_Flow/while/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* While*/
+
+int main()
+{
+    int count = 0;
+    while (count < 10)
+        {
+        std::cout << count << " ";
+        ++count;
+        }
+    std::cout << "done!";
+
+    return 0;
+}
