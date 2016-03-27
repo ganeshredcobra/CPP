22a83e999e413570f9ff8901534569abf10f0d48
ganeshredcobra <ganeshredcobra@gmail.com>
2016-03-28 00:27:10 +0530

    Pseudo Random Number Generator

---
+++ Control_Flow/PRNG/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+unsigned int PRNG()
+{
+    // our initial starting seed is 5323
+    static unsigned int seed = 5323;
+
+    // Take the current seed and generate a new value from it
+    // Due to our use of large constants and overflow, it would be
+    // very hard for someone to predict what the next number is
+    // going to be from the previous one.
+    seed = (8253729 * seed + 2396403);
+
+    // Take the seed and return a value between 0 and 32767
+    return seed  % 32768;
+}
+
+int main()
+{
+    // Print 100 random numbers
+    for (int count=0; count < 100; ++count)
+    {
+        std::cout << PRNG() << "\t";
+
+        // If we've printed 5 numbers, start a new row
+        if ((count+1) % 5 == 0)
+            std::cout << "\n";
+	}
+}
