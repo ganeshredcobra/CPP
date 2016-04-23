52b3cd706b32e38baedd5832e9db6fb440c8b9a1
ganeshredcobra <ganeshredcobra@gmail.com>
2016-04-23 22:53:57 +0530

    Std:Vector keeps track of its size

---
+++ Array_Pointer/std_Vector/vector_size/main.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+
+int main()
+{
+    std::vector<int> array { 9, 7, 5, 3, 1 };
+    std::cout << "The size is: " << array.size() << '\n';
+
+    return 0;
+}
