e134ceee305aeeb97fa79052a2caca663026a165
ganeshredcobra <ganeshredcobra@gmail.com>
2016-04-26 23:20:45 +0530

    Pass by Value

---
+++ Functions/Pass_by_val/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void foo(int y)
+{
+    using namespace std;
+    cout << "y = " << y << endl;
+}
+
+int main()
+{
+    foo(5); // first call
+
+    int x = 6;
+    foo(x); // second call
+    foo(x+1); // third call
+
+    return 0;
+}
