2c58bb85e66f79170542451010d7048c1370a7fd
ganeshredcobra <ganeshredcobra@gmail.com>
2016-04-29 23:41:05 +0530

    Pass by address Const

---
+++ Functions/pass_by_addr_const/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(const int *array, int length)
+{
+    // if user passed in a null pointer for array, bail out early!
+    if (!array)
+        return;
+
+    for (int index=0; index < length; ++index)
+        std::cout << array[index] << ' ';
+}
+
+int main()
+{
+    int array[6] = { 6, 5, 4, 3, 2, 1 };
+    printArray(array, 6);
+}
