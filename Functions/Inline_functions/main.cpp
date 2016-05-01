6f37af877a423216338e59eea8ac05f6e87e3ab0
ganeshredcobra <ganeshredcobra@gmail.com>
2016-05-02 00:01:35 +0530

    Inline Functions

---
+++ Functions/Inline_functions/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+inline int min(int x, int y)
+{
+    return x > y ? y : x;
+}
+
+int main()
+{
+    std::cout << min(5, 6) << '\n';
+    std::cout << min(3, 2) << '\n';
+    return 0;
+}
