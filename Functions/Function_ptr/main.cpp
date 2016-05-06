537ba5832a346908c5de7d0327dc6b9cdc80aa8b
ganeshredcobra <ganeshredcobra@gmail.com>
2016-05-06 23:30:58 +0530

    Function pointer example

---
+++ Functions/Function_ptr/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int foo()
+{
+    return 5;
+}
+
+int goo()
+{
+    return 6;
+}
+
+int main()
+{
+    int (*fcnPtr)() = foo; // fcnPtr points to function foo
+    fcnPtr = goo; // fcnPtr now points to function goo
+
+    return 0;
+}
