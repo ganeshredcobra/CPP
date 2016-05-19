3541643511e0b74dfdf8267633064f9e33b5d6b6
Ganesh <ganeshredcobra@gmail.com>
2016-05-19 14:46:42 +0530

    Default Constructor

---
+++ OOPS/Default_Constructor/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/*
+Constructors should always have the same name as the class (with the same capitalization)
+Constructors have no return type (not even void)
+*/
+ 
+class Fraction
+{
+private:
+    int m_numerator;
+    int m_denominator;
+ 
+public:
+    Fraction() // default constructor
+    {
+         m_numerator = 0;
+         m_denominator = 1;
+    }
+ 
+    int getNumerator() { return m_numerator; }
+    int getDenominator() { return m_denominator; }
+    double getValue() { return static_cast<double>(m_numerator) / m_denominator; }
+};
+ 
+int main()
+{
+    Fraction frac; // Since no arguments, calls Fraction() default constructor
+    std::cout << frac.getNumerator() << "/" << frac.getDenominator() << '\n';
+ 
+    return 0;
+}
