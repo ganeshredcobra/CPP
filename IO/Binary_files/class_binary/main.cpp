d67bc0f1f330566d9c140df3e648b375b1c65389
ganeshredcobra <ganeshredcobra@gmail.com>
2016-08-15 00:50:31 +0530

    Removed appending option

--- IO/Binary_files/class_binary/main.cpp
+++ IO/Binary_files/class_binary/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 
 string filename = "test.dat";
 
-class TAP_Data
+class P_Data
 {
 public:
-    float aPillarAngle;
+    float Angle;
     int View_X;
     int View_Y;
     int Offset_X;
@@ -30,12 +30,12 @@ void WriteTAPData(ofstream& of)
     }
     else
     {
-        fout.write((char *)&of,sizeof(TAP_Data));
+        fout.write((char *)&of,sizeof(P_Data));
         fout.close();
     }
 }
 
-void ReadTAPData(ifstream& inf, TAP_Data Read)
+void ReadTAPData(ifstream& inf, P_Data Read)
 {
     // Read from file
     ifstream fin(filename.c_str(), ios::in | ios::binary);
@@ -55,22 +55,20 @@ void ReadTAPData(ifstream& inf, TAP_Data Read)
 
 int main()
 {
-    TAP_Data ReadD;
-    TAP_Data Config_Data {95.76,73,72,59,58,75,76};
-    //WriteTAPData(filename);
-    //ReadTAPData(ReadD);
+    P_Data ReadD;
+    P_Data Config_Data {95.76,73,72,59,58,75,76};
 
    ofstream outfile;
-   outfile.open (filename.c_str(), ios::out|ios::binary|ios::app);
-   outfile.write ( (char *)(&Config_Data), sizeof(TAP_Data) );
+   outfile.open (filename.c_str(), ios::out|ios::binary);
+   outfile.write ( (char *)(&Config_Data), sizeof(P_Data) );
    outfile.close();
 
    ifstream infile;
    infile.open (filename.c_str(), ios::in|ios::binary);
 
-   while( infile.read ( (char *)&ReadD, sizeof(TAP_Data) ) )
+   while( infile.read ( (char *)&ReadD, sizeof(P_Data) ) )
    infile.close();
 
-    cout <<"Pillar angle is "<< ReadD.aPillarAngle << endl;
+    cout <<"Pillar angle is "<< ReadD.Angle << endl;
 
 }
