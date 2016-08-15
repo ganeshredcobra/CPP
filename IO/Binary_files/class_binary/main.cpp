132050c2615994ff9eaad3b76adb9b7da382c9e0
ganeshredcobra <ganeshredcobra@gmail.com>
2016-08-16 00:03:03 +0530

    Write Read and Load done

--- IO/Binary_files/class_binary/main.cpp
+++ IO/Binary_files/class_binary/main.cpp
@@ -17,58 +17,57 @@ public:
     int Offset_Y;
     int Crop_Height;
     int Crop_Width;
+
+    void Print_Data()
+    {
+        cout <<"Pillar angle is "<< this->Angle << endl;
+        cout <<"View_X is "<< this->View_X << endl;
+        cout <<"View_Y is "<< this->View_X << endl;
+        cout <<"Offset_X is "<< this->Offset_X << endl;
+        cout <<"Offset_X is "<< this->Offset_Y << endl;
+        cout <<"Crop_Height is "<< this->Crop_Height << endl;
+        cout <<"Crop_Width is "<< this->Crop_Width << endl;
+    }
 };
 
-void WritePData(ofstream& of)
+void WritePData(P_Data& Config_Data)
 {
     // Write to File
-    ofstream fout(filename.c_str(), ios::out | ios::binary);
-    if (!fout.is_open())
-    {
-        cerr << "error: open file for output failed!" << endl;
-        abort();
-    }
-    else
-    {
-        fout.write((char *)&of,sizeof(P_Data));
-        fout.close();
-    }
+    ofstream outfile;
+    outfile.open (filename.c_str(), ios::out|ios::binary);
+    outfile.write ( (char *)(&Config_Data), sizeof(P_Data) );
+    outfile.close();
 }
 
-void ReadPData(ifstream& inf, P_Data Read)
+void ReadPData(P_Data& ReadD)
 {
     // Read from file
-    ifstream fin(filename.c_str(), ios::in | ios::binary);
-    if (!fin.is_open())
-    {
-        cerr << "error: open file for input failed!" << endl;
-        abort();
-    }
-    else
-    {
-        //fout.read((char *)&inf,sizeof(TAP_Data));
-        //inf.read(Read.aPillarAngle,sizeof(aPillarAngle));
 
+    ifstream infile;
+    infile.open (filename.c_str(), ios::in|ios::binary);
 
-    }
+    while( infile.read ( (char *)&ReadD, sizeof(P_Data) ) )
+        infile.close();
+}
+
+void LoadData(P_Data& SrcRecord, P_Data& NewRecord)
+{
+    SrcRecord = NewRecord;
 }
 
 int main()
 {
     P_Data ReadD;
-    P_Data Config_Data {95.76,73,72,59,58,75,76};
+    P_Data Config_Data = {97.76,73,72,59,58,75,76};
 
-   ofstream outfile;
-   outfile.open (filename.c_str(), ios::out|ios::binary);
-   outfile.write ( (char *)(&Config_Data), sizeof(P_Data) );
-   outfile.close();
+    WritePData(Config_Data);
+    ReadPData(ReadD);
 
-   ifstream infile;
-   infile.open (filename.c_str(), ios::in|ios::binary);
+    Config_Data.Print_Data();
 
-   while( infile.read ( (char *)&ReadD, sizeof(P_Data) ) )
-   infile.close();
+    ReadD = {76,76,76,76,76,76,76};
 
-    cout <<"Pillar angle is "<< ReadD.Angle << endl;
+    LoadData(Config_Data, ReadD);
 
+    Config_Data.Print_Data();
 }
