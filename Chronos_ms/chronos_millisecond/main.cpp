680d2e842da4866b5c7fbef31d77843bc9f0d461
ganeshredcobra <ganeshredcobra@gmail.com>
2016-09-12 01:47:14 +0530

    Chronos millisecond timer code

---
+++ Chronos_ms/chronos_millisecond/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unistd.h>
+#include <chrono>
+
+#define TIMING
+
+#ifdef TIMING
+#define INIT_TIMER auto start = std::chrono::high_resolution_clock::now();
+#define START_TIMER  start = std::chrono::high_resolution_clock::now();
+#define STOP_TIMER(name)  std::cout << "RUNTIME of " << name << ": " << \
+    std::chrono::duration_cast<std::chrono::milliseconds>( \
+            std::chrono::high_resolution_clock::now()-start \
+    ).count() << " ms " << std::endl;
+#else
+#define INIT_TIMER
+#define START_TIMER
+#define STOP_TIMER(name)
+#endif
+
+int main() {
+    INIT_TIMER
+    START_TIMER
+    sleep(2);
+    STOP_TIMER("sleeping for 2 seconds")
+    START_TIMER
+    long unsigned int b = 0;
+    for(int i = 0; i < 10000000; i++) {
+        b += i;
+    }
+    STOP_TIMER("some long loop")
+}
